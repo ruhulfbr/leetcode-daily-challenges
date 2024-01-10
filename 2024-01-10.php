7346c1f65e0098150b979b7bac47bf437ba8cde6
Md. Ruhul Amin <ruhul11bd@gmail.com>
2024-01-10 11:58:03 +0600

    Create 2024-01-10.php

---
+++ 2024-01-10.php
@@ -0,0 +1,61 @@
+<?php
+
+// 2385. Amount of Time for Binary Tree to Be Infected
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $start
+     * @return Integer
+     */
+    
+    public $g = [];
+
+    public function amountOfTime($root, $start) {
+        $this->dfs($root);
+        return $this->dfsHelper($start, -1);
+    }
+
+    public function dfsHelper($i, $fa) {
+        $ans = 0;
+        foreach ($this->g[$i] as $j) {
+            if ($j != $fa) {
+                $ans = max($ans, 1 + $this->dfsHelper($j, $i));
+            }
+        }
+        return $ans;
+    }
+
+    public function dfs($root) {
+        if (!$root) {
+            return;
+        }
+
+        if ($root->left) {
+            $this->g[$root->val][] = $root->left->val;
+            $this->g[$root->left->val][] = $root->val;
+        }
+
+        if ($root->right) {
+            $this->g[$root->val][] = $root->right->val;
+            $this->g[$root->right->val][] = $root->val;
+        }
+
+        $this->dfs($root->left);
+        $this->dfs($root->right);
+    }
+}
